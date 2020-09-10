4c8ebbb72e96a0764c7e6e927eb1812dff18d13c
Theo <mrantistupid@gmail.com>
2020-09-10 17:05:24 -0400

    Fixed .setPath for example

--- java/example/Main.java
+++ java/example/Main.java
@@ -5,7 +5,7 @@ import gdf.GDFReader;
 
 public class Main {
 	public static void main(String[] args) {
-		GDFReader.setPath("src/test/");
+		GDFReader.setPath("src/example/");
 		GDFData data = GDFReader.getData("myFile", "data");
 		System.out.println(data.getEnum("ele", Element.class));
 	}
